1c6aaefcd6760831e1ff707aaa084d223206b6e9
Sarvesh <sarvesh4232@gmail.com>
2020-09-21 22:12:41 +0530

    Imagekit full test

---
+++ assets/js/worker.js
@@ -0,0 +1,2 @@
+// copy-paste the below line inside your service worker code loaded with the root scope
+importScripts("https://runtime.imagekit.io/<your_imagekit_id>/v1/js/network-based-adaption.js?v=");
